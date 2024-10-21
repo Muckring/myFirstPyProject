8ea578ac300a297f10fe4f8edbb7f4f257733daf
Konstantin <zakharov83@mail.ru>
2024-10-21 18:01:44 +0300

    added some content to variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Variables are containers for values
+# that behave as the values they contain
+
+first_name = "Kostya"
+last_name = "Zakharov"
+age = 41
+isHuman = True
+
+print(first_name)
+print(last_name)
+print(age)
+print(isHuman)
