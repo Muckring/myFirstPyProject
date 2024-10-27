45e385338079b8e9be78388ca98efaeae418602c
Konstantin <zakharov83@mail.ru>
2024-10-27 17:40:49 +0300

    added some content to typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+name = 'Kostya'
+age = 23
+gpa = 21.4
+student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
+
+name = bool(name)
+age = float(age)
+gpa = int(gpa)
+student = str(student)
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
