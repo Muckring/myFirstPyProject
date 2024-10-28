c3a0d02a29de33645805ee1953997ed7d25df830
Konstantin <zakharov83@mail.ru>
2024-10-28 20:49:45 +0300

    commented out some code

--- logex.py
+++ logex.py
@@ -3,18 +3,18 @@
 # result = "It is sunny" if sunny  else "It is cloudy"
 # print(result)
 
-is_student = True
+# is_student = True
 
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-if age < 18:
-    is_student = False
-    print("Access Denied. You are too young")
-    print(is_student)
-elif age > 50:
-    is_student = False
-    print("Access Denied. You are too old")
-    print(is_student)
-else:
-    print("Access Granted")
-    print(is_student)
+# if age < 18:
+#     is_student = False
+#     print("Access Denied. You are too young")
+#     print(is_student)
+# elif age > 50:
+#     is_student = False
+#     print("Access Denied. You are too old")
+#     print(is_student)
+# else:
+#     print("Access Granted")
+#     print(is_student)
