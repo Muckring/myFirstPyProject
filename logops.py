b13996149c8dd7d1635cce7c01f8cc0f50e005fb
Konstantin <zakharov83@mail.ru>
2024-10-28 17:28:36 +0300

    added some stuff

---
+++ logops.py
@@ -0,0 +1,20 @@
+# sunny = True
+
+# result = "It is sunny" if sunny  else "It is cloudy"
+# print(result)
+
+is_student = True
+
+age = int(input("Enter your age: "))
+
+if age < 18:
+    is_student = False
+    print("Access Denied. You are too young")
+    print(is_student)
+elif age > 50:
+    is_student = False
+    print("Access Denied. You are too old")
+    print(is_student)
+else:
+    print("Access Granted")
+    print(is_student)
