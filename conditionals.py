cf3e24ff78037201fca17ec8ef825f22f5497960
Konstantin <zakharov83@mail.ru>
2024-10-28 15:25:47 +0300

    added some crap to new file

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+name = str(input("Enter your name: "))
+
+# if name == "":
+#     print("You haven't entered your name")
+# else:
+#     print(f"Hello, {name}!")
+
+while name == "":
+    input("Enter your name: ")
+    
+    
+if name != "
+":
+    print(f"Hello, {name}")
