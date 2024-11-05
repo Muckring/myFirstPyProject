46b96e4bed6eb90f93497750bb5276c617ecfe8b
Konstantin <zakharov83@mail.ru>
2024-11-05 18:08:19 +0300

    added content to format_specifiers

---
+++ format_specifiers.py
@@ -0,0 +1,9 @@
+number1 = 3.455678
+number2 = -456.999
+number3 = 45.66
+
+# add spaces add a colon after the value and specify the number
+# spaces
+
+print(f"Number One is {number1:<+,.5f}")
+print(f"Number Two is {number2:>}")
