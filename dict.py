1a9b91b688d1c03b011bd56603df8b4ef46b9d66
Konstantin <zakharov83@mail.ru>
2024-11-09 19:25:31 +0300

    added some methods to dict.py

--- dict.py
+++ dict.py
@@ -4,22 +4,72 @@ capitals: dict = {"Russia" : "Moscow",
                   "Ukraine": "Kyiv"}
 
 
-keys : object = capitals.keys()
-print(keys)
-for key in keys:
-    print(key, end=" ")
+# keys : object = capitals.keys()
+# print(keys)
+# for key in keys:
+#     print(key, end=" ")
 
-print(" ")
+# print(" ")
 
-values : object = capitals.values()
-print(values)
-for value in values:
-    print(value, end=" ")
+# values : object = capitals.values()
+# print(values)
+# for value in values:
+#     print(value, end=" ")
 
-print(" ")
+# print(" ")
 
-items = capitals.items()
-print(items)
-for key, value in capitals.items():
-    print(f"{key}: {value}")
-    
+# items = capitals.items()
+# print(items)
+# for key, value in capitals.items():
+#     print(f"{key}: {value}")
+
+
+
+# You can use update() to add a new key-value pair to the 
+# end of the dictionary
+
+capitals.update({"France" : "Paris"})
+print(capitals)
+
+# Or, you can modify the existing key-value pair
+
+capitals.update({"Russia" : "Mukhosransk"})
+print(capitals)
+
+#You can also change an item as follows:
+capitals["France"] = "Marseiile"
+print(capitals)
+
+# You can use pop() to remove an item and specify the key
+# capitals.pop("Russia")
+# print(capitals)
+
+# popitem() removes the last item in the dict
+# capitals.popitem()
+# print(capitals)
+
+# You can use either copy() or dict() constructor
+# to copy a dict
+
+# new_dict = capitals.copy()
+# print(new_dict)
+
+# dict2 = dict(capitals)
+# print(dict2)
+
+child1 = {
+    "name" : "Kostya",
+    "year" : 1983
+}
+
+child2 = {
+    "name" : "Igor",
+    "year" : 1992
+}
+
+siblings = {
+    "brother1" : child1,
+    "brother2" : child2
+}
+
+print(siblings)
