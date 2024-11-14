5a7a36ca7f0fb3bc1686ef2dee50fb7aa1ea60e9
Konstantin <zakharov83@mail.ru>
2024-11-14 16:49:19 +0300

    updated main.py and module.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from module import *
+
+def func2() -> None:
+    print("This is the main function")
+
+def main():
+    func1()
+    func2()
+
+if __name__ == "__main__":
+    main()
---
+++ module.py
@@ -0,0 +1,2 @@
+def func1() -> None:
+    print("Hello from func1")
