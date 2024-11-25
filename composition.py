4fe3612ec53c68fa0aa97a6b9971e560e705d224
Konstantin <zakharov83@mail.ru>
2024-11-25 18:38:25 +0300

    updated composition.py with some content

--- composition.py
+++ composition.py
@@ -1,1 +1,22 @@
 # This is a lesson in composition
+
+
+class Hand():
+    def __init__(self, quality: str, finger_size: int):
+        self.quality = quality
+        self.fingers = [Finger(finger_size) for finger in range(5)]
+
+    def display_hand(self):
+        return f'{self.quality} {self.fingers[0].length}cm hand'
+
+
+class Finger():
+    
+    def __init__(self, length: int):
+        self.length = length
+        
+   
+
+hand = Hand('rugged', 23)
+
+print(hand.display_hand())
